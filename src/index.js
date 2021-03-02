140bb0c0ce3f1537eab4e466da237193df11204b
Olga Matyashovska <matyashowskamaria@gmail.com>
2021-03-02 21:48:23 +0200

    feat: implement towel-sort using array methods

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let array = [];
+  if(matrix && matrix.length){
+    let towelSorted = (matrix.map((item,index) => (index % 2 == 0) ? item : item.reverse())).join(",").split(",");
+    array = towelSorted;
+    return array;
+  }
+  else{
+    return array;
+  }
 }
